b46b949d4e41557e494f1587a11a01e17b943a03
james <james@ubuntu-laptop.meobets.com>
2010-05-02 00:31:34 -0700

    - Updated Outlet class generator to ignore classes which already exist

--- lib/outlet-0.7/classes/outlet/OutletClassGenerator.php
+++ lib/outlet-0.7/classes/outlet/OutletClassGenerator.php
@@ -1,18 +1,19 @@
 <?
 class OutletClassGenerator {
-	private $config;
+  private $config;
 
-	function __construct (OutletConfig $config) {
-		$this->config = $config;
-	}
+  function __construct (OutletConfig $config) {
+    $this->config = $config;
+  }
 
   function generate() {
     $c = "";
-		foreach ($this->config->getEntities() as $entity) {
-			$clazz = $entity->getClass();
-      $c .= "class $clazz {\n";
+    foreach ($this->config->getEntities() as $entity) {
+      $clazz = $entity->getClass();
+      $c .= "if (!class_exists(\"$clazz\")) {\n";
+      $c .= "  class $clazz {\n";
       foreach ($entity->GetProperties() as $k=>$v) {
-        $c .= "  public \$$k";
+        $c .= "    public \$$k";
 
         if (!empty($v["default"]))
           $c .= ' = "' . $v["default"] . '"';
@@ -20,85 +21,86 @@ class OutletClassGenerator {
         $c .= ";\n";
       }
 
-			foreach ($entity->getAssociations() as $assoc) {
-				switch ($assoc->getType()) {
-					case 'one-to-many': $c .= $this->createOneToManyFunctions($assoc); break;
+      foreach ($entity->getAssociations() as $assoc) {
+        switch ($assoc->getType()) {
+          case 'one-to-many': $c .= $this->createOneToManyFunctions($assoc); break;
           case 'many-to-one': $c .= $this->createManyToOneFunctions($assoc); break;
-          case 'one-to-one':	$c .= $this->createOneToOneFunctions($assoc); break;
-          case 'many-to-many':	$c .= $this->createManyToManyFunctions($assoc); break;
+          case 'one-to-one':  $c .= $this->createOneToOneFunctions($assoc); break;
+          case 'many-to-many':  $c .= $this->createManyToManyFunctions($assoc); break;
           //default: throw new Exception("invalid association type: {$assoc->getType()}");
-				}
-			}
+        }
+      }
+      $c .= "  }\n";
       $c .= "}\n";
     }
 
     return $c;
   }
-	function createOneToManyFunctions (OutletAssociationConfig $config) {
-		$foreign	= $config->getForeign();
-		$foreignplural	= $config->getForeignPlural();
-		$key 		= $config->getKey();
-		$pk_prop 	= $config->getRefKey();
-		$getter		= $config->getGetter();
-		$setter		= $config->getSetter();
-	
-		$c = '';
+  function createOneToManyFunctions (OutletAssociationConfig $config) {
+    $foreign  = $config->getForeign();
+    $foreignplural  = $config->getForeignPlural();
+    $key     = $config->getKey();
+    $pk_prop   = $config->getRefKey();
+    $getter    = $config->getGetter();
+    $setter    = $config->getSetter();
+  
+    $c = '';
     $c .= "  private \$" . strtolower($foreignplural) . ";\n";
-		$c .= "  public function $getter() { \n";
-		$c .= "    return \$this->" . strtolower($foreignplural) . "; \n";
-		$c .= "  } \n";
-		$c .= "  public function $setter(Collection \$ref".($config->isOptional() ? '=null' : '').") { \n";
-		$c .= "    \$this->" . strtolower($foreignplural) . " = \$ref; \n";
-		$c .= "  } \n";
+    $c .= "  public function $getter() { \n";
+    $c .= "    return \$this->" . strtolower($foreignplural) . "; \n";
+    $c .= "  } \n";
+    $c .= "  public function $setter(Collection \$ref".($config->isOptional() ? '=null' : '').") { \n";
+    $c .= "    \$this->" . strtolower($foreignplural) . " = \$ref; \n";
+    $c .= "  } \n";
 
-		return $c;
-	}
+    return $c;
+  }
 
-	function createManyToOneFunctions (OutletAssociationConfig $config) {
-		$foreign	= $config->getForeign();
-		$foreignplural	= $config->getForeignPlural();
-		$key 		= $config->getKey();
-		$pk_prop 	= $config->getRefKey();
-		$getter		= $config->getGetter();
-		$setter		= $config->getSetter();
-	
-		$c = '';
+  function createManyToOneFunctions (OutletAssociationConfig $config) {
+    $foreign  = $config->getForeign();
+    $foreignplural  = $config->getForeignPlural();
+    $key     = $config->getKey();
+    $pk_prop   = $config->getRefKey();
+    $getter    = $config->getGetter();
+    $setter    = $config->getSetter();
+  
+    $c = '';
     $c .= "  private \$" . strtolower($foreign) . ";\n";
-		$c .= "  public function $getter() { \n";
-		$c .= "    return \$this->" . strtolower($foreign) . "; \n";
-		$c .= "  } \n";
-		$c .= "  public function $setter($foreign \$ref".($config->isOptional() ? '=null' : '').") { \n";
-		$c .= "    \$this->" . strtolower($foreign) . " =& \$ref; \n";
-		$c .= "  } \n";
+    $c .= "  public function $getter() { \n";
+    $c .= "    return \$this->" . strtolower($foreign) . "; \n";
+    $c .= "  } \n";
+    $c .= "  public function $setter($foreign \$ref".($config->isOptional() ? '=null' : '').") { \n";
+    $c .= "    \$this->" . strtolower($foreign) . " =& \$ref; \n";
+    $c .= "  } \n";
 
-		return $c;
-	}
-	function createOneToOneFunctions (OutletAssociationConfig $config) {
-		$foreign	= $config->getForeign();
-		$key 		= $config->getKey();
-		$getter 	= $config->getGetter();
-		$setter		= $config->getSetter();
+    return $c;
+  }
+  function createOneToOneFunctions (OutletAssociationConfig $config) {
+    $foreign  = $config->getForeign();
+    $key     = $config->getKey();
+    $getter   = $config->getGetter();
+    $setter    = $config->getSetter();
 
     return '';
   }
-	function createManyToManyFunctions (OutletManyToManyConfig $config) {
-		$foreign	= $config->getForeign();
-		$foreignplural	= $config->getForeignPlural();
-		$key 		= $config->getKey();
-		$pk_prop 	= $config->getRefKey();
-		$getter		= $config->getGetter();
-		$setter		= $config->getSetter();
-		$table		= $config->getLinkingTable();
-		$otherKey	= $config->getOtherKey();
+  function createManyToManyFunctions (OutletManyToManyConfig $config) {
+    $foreign  = $config->getForeign();
+    $foreignplural  = $config->getForeignPlural();
+    $key     = $config->getKey();
+    $pk_prop   = $config->getRefKey();
+    $getter    = $config->getGetter();
+    $setter    = $config->getSetter();
+    $table    = $config->getLinkingTable();
+    $otherKey  = $config->getOtherKey();
 
-		$c = '';
+    $c = '';
     $c .= "  private \$" . strtolower($foreignplural) . ";\n";
-		$c .= "  public function $getter() { \n";
-		$c .= "    return \$this->" . strtolower($foreignplural) . "; \n";
-		$c .= "  } \n";
-		$c .= "  public function $setter(Collection \$ref".($config->isOptional() ? '=null' : '').") { \n";
-		$c .= "    \$this->" . strtolower($foreignplural) . " = \$ref; \n";
-		$c .= "  } \n";
+    $c .= "  public function $getter() { \n";
+    $c .= "    return \$this->" . strtolower($foreignplural) . "; \n";
+    $c .= "  } \n";
+    $c .= "  public function $setter(Collection \$ref".($config->isOptional() ? '=null' : '').") { \n";
+    $c .= "    \$this->" . strtolower($foreignplural) . " = \$ref; \n";
+    $c .= "  } \n";
 
     return $c;
   }

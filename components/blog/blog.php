d2aed33b3285d734027863f77b0d21ed9b8e14a4
james <james@ubuntu-laptop.meobets.com>
2010-05-03 03:34:16 -0700

    - Blog component improvements

--- components/blog/blog.php
+++ components/blog/blog.php
@@ -3,45 +3,71 @@
 class Component_blog extends Component {
   function init() {
     $this->conn = Outlet::getInstance();
-    $this->conn->createProxies();
+    $orm = OrmManager::singleton();
+    $orm->LoadModel("blog");
   }
 
   function controller_blog($args, $output="inline") {
-    $vars["args"] = $args;
-    $vars["blogname"] = $args["blogname"];
+    $ret = $this->GetComponentResponse("./blog.tpl");
 
-    try {
-      $vars["blog"] = $this->conn->load("Blog", $vars["blogname"]);
-    } catch(Exception $e) {
-      print_pre($e->getMessage());
-    }
-
+    $ret["blogs"] = $this->conn->select("Blog");
+/*
     if (!empty($vars["blog"])) {
       $ret = $this->GetTemplate("./blog.tpl", $vars);
     } else {
       $ret = $this->GetTemplate("./blog_notfound.tpl", $vars);
     }
+*/
     return $ret;
   }
 
-  function controller_create($args, $output="inline") {
-    $vars["args"] = $args;
-    
-    $vars["blog"] = new Blog;
-    $vars["blog"]->blogname = "supercritical";
-    $vars["blog"]->title = "Supercritical Industries, LLC";
-    $vars["blog"]->subtitle = "Our Business is Serious Business";
-    $vars["blog"]->owner = "james";
-    
-    try {
-      $this->conn->save($vars["blog"]);
-      $vars["success"] = true;
-    } catch(Exception $e) {
-      $vars["success"] = false;
-      print_pre($e);
+  function controller_summary($args) {
+    $ret = $this->GetComponentResponse("./summary.tpl");
+    if (!empty($args["blogname"])) {
+      $ret["blogname"] = $args["blogname"];
+      $ret["blog"] = $this->conn->load("Blog", $ret["blogname"]);
+    } else if (!empty($args["blog"])) {
+      $ret["blog"] = $args["blog"];
+      $ret["blogname"] = $ret["blog"]->blogname;
+    }
+    return $ret;
+  }
+
+  function controller_view($args) {
+    $ret = $this->GetComponentResponse("./view.tpl");
+    if (!empty($args["blogname"])) {
+      $ret["blogname"] = $args["blogname"];
+
+      try {
+        $ret["blog"] = $this->conn->load("Blog", $ret["blogname"]);
+      } catch(Exception $e) {
+        print_pre($e->getMessage());
+      }
     }
 
-    return $this->GetTemplate("./create.tpl", $vars);
+    return $ret;
+  }
+  function controller_create($args, $output="inline") {
+    $vars = $this->GetComponentResponse("./create.tpl");
+    
+    if (!empty($args["blog"])) {
+      $vars["blog"] = new Blog;
+      $vars["blog"]->blogname = $args["blog"]["blogname"];
+      $vars["blog"]->title = $args["blog"]["title"];
+      $vars["blog"]->subtitle = $args["blog"]["subtitle"];
+      $vars["blog"]->owner = $args["blog"]["owner"];
+    
+      try {
+        $this->conn->save($vars["blog"]);
+        $vars["success"] = true;
+        header("Location: /blog#blog_create_success:" . $vars["blog"]->blogname);
+      } catch(Exception $e) {
+        $vars["success"] = false;
+        print_pre($e);
+      }
+    }
+
+    return $vars;
   }
 } 
 

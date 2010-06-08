8505fbe9027666eddc57f564f55d3ba3f3ff8d9c
Lucian <lucian303@gmail.com>
2010-06-07 19:41:19 -0700

    Adding autoload stuff

--- htdocs/go.php
+++ htdocs/go.php
@@ -1,9 +1,13 @@
-<?
+<?php
 $root = preg_replace("|/htdocs$|", "", getcwd());
 ini_set("include_path", ".:$root/include:/usr/share/php");
 
 putenv('TZ=America/Los_Angeles');
 chdir($root);
+
+//For Zend
+$path = "$root/lib" . PATH_SEPARATOR . "$root/include/forms";
+set_include_path($path . PATH_SEPARATOR . get_include_path());
 
 include_once("include/webapp_class.php");
 include_once("lib/profiler.php");

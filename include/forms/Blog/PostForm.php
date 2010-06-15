1dfbb959c39e969eb0a8aa8ff0699894a7e90270
Lucian <lucian303@gmail.com>
2010-06-15 13:56:52 -0700

    reworked form ... going to refactor for simplicity and usability

--- include/forms/Blog/PostForm.php
+++ include/forms/Blog/PostForm.php
@@ -3,15 +3,17 @@ class Blog_PostForm
 {
   public function getForm($args)
   {
-    $formName = @any($args["formname"], "htmlform");
-    $dispatchName = @any($args["dispatchname"], $formName);
+    $formName = any($args["formname"], "htmlform");
+    $dispatchName = any($args["dispatchname"], $formName);
 
     $form = new Elation_Form(array('file' => $args['modelFile'], 'class' => $args['modelClass']), $args['formConfigType']);
     
+		/*
     $hiddenDispatch = new Zend_Form_Element_Hidden(array("name" => $dispatchName, "value" => $args["formhandler"]));
     $hiddenDispatch->setBelongsTo('_predispatch');
     $form->addElement($hiddenDispatch);
-    
+    */
+		
     if(array_key_exists('postCreateCallback', $args) && array_key_exists('class',  $args['postCreateCallback']) && array_key_exists('method',  $args['postCreateCallback'])){
       $params = NULL;
       if(array_key_exists('params',  $args['postCreateCallback'])) {

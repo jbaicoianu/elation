3ab9721b287b92c752cc30afdaae923669fadf4f
James Lazarus <lazarus@b08.fatlens.com>
2010-12-15 19:37:36 -0800

    buh

---
+++ include/jstemplatemanager_class.php
@@ -0,0 +1,31 @@
+<?
+
+class JSTemplateManager extends Base {
+  protected static $instance;
+  private $templates = array();
+
+  public function __construct() {
+  }
+  function SetTemplate($tplname, $tplstr) {
+    $this->templates[$tplname] = json_encode($tplstr);
+  }
+  function HasTemplate($tplname) {
+    return isset($this->templates[$tplname]);
+  }
+  function GetTemplates() {
+    return $this->templates;
+  }
+
+  public static function singleton($args=NULL) { 
+    $name = __CLASS__; 
+    if (!self::$instance) {
+      if (! empty($args)) {
+        self::$instance = new $name($args);
+      } else {
+        self::$instance = null;
+      }
+    } 
+    return self::$instance; 
+  }
+
+}

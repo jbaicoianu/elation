ce2008387230890b07f73389af0948b1a953693f
James Lazarus <lazarus@b08.fatlens.com>
2010-12-23 05:43:57 -0800

    stuff

--- components/utils/htdocs/scripts/panel.js
+++ components/utils/htdocs/scripts/panel.js
@@ -32,19 +32,19 @@ elation.extend('panel', new function(options) {
 					panel.items[k] = new elation.panel.add_item(k, v, panel);
 				});
 			}
-			
+
 			if (panel.cfg.jsobject && panel.cfg.jsobject != "false") {
 				var obj = eval(panel.cfg.jsobject);
 				
 				if (typeof obj == 'function') {
 					panel.jsobj = new obj(this, panel);
 					elation[panel.name] = panel.jsobj;
-          
+
           if (typeof panel.jsobj.init == 'function')
             panel.jsobj.init();
 				}
 			}
-			
+
 			if (panel.cfg.navigation == "true") {
 				panel.container = document.getElementById(panel.cfg.targetid);
 				this.lis = $('div.tf_utils_panel_' + name + ' ul li');;
@@ -348,226 +348,4 @@ elation.extend('panel', new function(options) {
 		
     this.init();
   }
-});
-
-elation.extend('myfinds.picker', function(parent, panel) {
-	this.parent = parent;
-	this.panel = panel;
-	this.args = { mode: 'store' };
-	this.done = {};
-	this.selected = { brand: [], store: [] };
-	this.selected_ids = { brand: {}, store: {} };
-	
-	this.init = function() {
-		this.content = document.getElementById('tf_picker_tab_content');
-		this.search_input = document.getElementById('tf_picker_search_input');
-		this.btnCancel = document.getElementById('tf_picker_cancel');
-		this.btnDone = document.getElementById('tf_picker_done');
-		this.modeul = document.getElementById('tf_picker_mode');
-		this.modeli = elation.utils.getOnly(this.modeul, 'li');
-		this.catul = document.getElementById('tf_picker_category');
-		this.catli = elation.utils.getOnly(this.catul, 'li');		
-		
-		elation.ui.infobox.get('lightbox').dragging = false;
-		
-		for (var i=0; i<this.modeli.length; i++) {
-			var li = this.modeli[i],
-					input = li.getElementsByTagName('input')[0];
-			
-			elation.events.add(input, 'change', this);
-		}
-		
-		if (googleAnalytics) {
-			var storebrand = document.getElementById('tf_picker_radio_stores').checked ? 'store' : 'brand';
-			
-			googleAnalytics.trackPageViewWrapper('/virt_slm/personalize/storebrandpicker/'+storebrand+'/');
-		}
-		
-		elation.events.add(this.search_input, 'keyup,blur', this);
-		elation.events.add([ this.btnCancel, this.btnDone ], 'click', this);
-	}
-	
-	this.add = function(mode, category, items) {
-		var div = document.getElementById('tf_picker_tab_content'),
-				inputs = div.getElementsByTagName('input');
-		
-		if (!items)
-			return;
-		
-		this.items = {};
-		
-		if (mode) {
-			this.args.mode = mode;
-			
-			if (!this.default_mode)
-				this.default_mode = mode;
-		}
-		
-		if (category) {
-			this.category = category;
-			
-			if (googleAnalytics) {
-				var gacat = category.toLowerCase();
-						secat = gacat.split(' ')[0];
-				
-				if (secat != 'search') {
-					this.search_input.value = '';
-					googleAnalytics.trackEvent([ 'sbpicker', 'tab', gacat ]);
-				}
-			}
-		}
-		
-		for (var i=0; i<items.length; i++) {
-			var item = items[i],
-					id = 'tf_picker_checkbox_' + item.id;
-			
-			if (this.selected_ids[mode][id]) {
-				var input = document.getElementById(id);
-						parent = elation.utils.getParent(input, 'li');
-				
-				input.checked = true;
-				elation.utils.addclass(parent, 'selected');
-			}
-			
-			this.items[id] = item;
-		}
-		
-		elation.events.add(inputs, 'change', this);
-	}
-	
-	this.handleEvent = function(event) {
-		var event = event || window.event,
-				target = event.target || event.srcElement;
-		
-		if (typeof this[event.type] == 'function')
-			return this[event.type](event, target);
-	}
-	
-	this.blur = function(event, target) {
-		if (googleAnalytics) {
-			var mode = this.args.mode.toLowerCase(),
-					term = target.value;
-			
-			if (term && term != this.term) 
-				googleAnalytics.trackEvent([ 'sbpicker', mode + '_search', term ]);
-			
-			this.term = term;
-		}
-	}
-	
-	this.keyup = function(event, target) {
-		var parms = 'targetid=tf_picker_tab_content&tab='+(this.panel.active_argname || 1)+
-				'&mode='+this.args.mode+'&search='+target.value;
-		
-		ajaxlib.Queue({
-			method: 'GET', 
-			url: '/myfinds/picker_content',
-			args: parms,
-			callback: [
-				this,
-				function(response) {
-					//console.log('response',response);
-				}
-			]
-		});
-	}
-	
-	this.click = function(event, target) {
-		if (target == this.btnCancel)
-			elation.lightbox.hide();
-		
-		var sent = { store: 0, brand: 0 },
-				request = {},
-				e = encodeURIComponent,
-				s = JSON.stringify;
-		
-		for (var key in this.selected) {
-			var itemid = '',
-					listname = 'saved_'+key+'s',
-					itemtype = key;
-			
-			for (var i=0; i<this.selected[key].length; i++) {
-				var item = this.selected[key][i],
-						id = key == 'brand' ? item.display_name : item.id;
-				
-				itemid += (itemid ? ',' : '') + id;
-				
-				sent[key] = i + 1;
-			}
-			
-			if (itemid) {
-				request[key] = {				
-					itemid: itemid, 
-					itemtype: itemtype, 
-					listname: listname
-				}
-			}
-		}
-		
-		if (sent['store'] || sent['brand']) {
-			if (googleAnalytics) {
-				if (sent['store'] > 0) {
-					googleAnalytics.trackEvent([ 'sbpicker', 'store', 'save' ]);
-				} if (sent['brand'] > 0) {
-					googleAnalytics.trackEvent([ 'sbpicker', 'brand', 'save' ]);
-				}
-			}
-			
-			ajaxlib.Queue({
-				method: 'POST', 
-				url: '/myfinds/picker_save',
-				args: 'scount='+sent['store']+'&bcount='+sent['brand']+'&json='+e(s(request)),
-				callback: [
-					this,
-					function() {
-						var tabs = $("ul.tf_myfinds_page_main_tabs").data("tabs");
-						
-						if (this.default_mode == 'store') {
-							ajaxlib.Get('/myfinds/page_stores_brands?pagetype=saved_stores');
-						} else if (this.default_mode == 'brand') {
-							ajaxlib.Get('/myfinds/page_stores_brands?pagetype=saved_brands');
-						}
-					}
-				]
-			});
-		} else
-			elation.lightbox.hide();
-	}
-	
-	this.change = function(event, target) {
-		var parent = elation.utils.getParent(target, 'li');
-		
-		if (parent == this.modeli[0] || parent == this.modeli[1]) {
-			if (parent == this.modeli[0]) 
-				this.args.mode = 'store';
-			else
-				this.args.mode = 'brand';
-			
-			this.search_input.value = '';
-			this.parent.load_tab_content(this.panel, this.catli[0]);
-			
-			return;
-		}
-		
-		var item = this.items[target.id],
-				sel = this.selected[this.args.mode],
-				selid = this.selected_ids[this.args.mode];
-		
-		if (item) {
-			if (elation.html.hasclass(parent, 'selected')) {
-				elation.utils.removeclass(parent, 'selected');
-				sel.splice(item.added_index, 1);
-				delete item.added_index;
-			} else {
-				item.added_index = sel.length;
-				sel.push(item);
-				selid[target.id] = true;
-				elation.utils.addclass(parent, 'selected');
-			}
-		}
-		
-		return false;
-	}
-	
-	//this.init();
 });

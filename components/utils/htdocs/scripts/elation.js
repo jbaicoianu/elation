87de0c521a5c3d954c5d7d25505a7cb4cbdb233b
James Lazarus <lazarus@b08.fatlens.com>
2011-01-12 18:45:53 -0800

    stuff

--- components/utils/htdocs/scripts/elation.js
+++ components/utils/htdocs/scripts/elation.js
@@ -537,6 +537,180 @@ elation.extend("utils.stringify", function() {
   return ret.substr(0,ret.length-1);
 });
 
+elation.extend("utils.htmlentities", function(string, quote_style) {
+	// http://kevin.vanzonneveld.net
+  var histogram = {}, symbol = '', tmp_str = '', entity = '';
+	tmp_str = string.toString();
+	
+	if (false === (histogram = elation.utils.get_html_translation_table('HTML_ENTITIES', quote_style))) {
+			return false;
+	}
+	
+	for (symbol in histogram) {
+			entity = histogram[symbol];
+			tmp_str = tmp_str.split(symbol).join(entity);
+	}
+	
+	return tmp_str;
+});
+elation.extend("utils.get_html_translation_table", function(table, quote_style) {
+	// http://kevin.vanzonneveld.net
+  var entities = {}, histogram = {}, decimal = 0, symbol = '';
+	var constMappingTable = {}, constMappingQuoteStyle = {};
+	var useTable = {}, useQuoteStyle = {};
+	
+	useTable      = (table ? table.toUpperCase() : 'HTML_SPECIALCHARS');
+	useQuoteStyle = (quote_style ? quote_style.toUpperCase() : 'ENT_COMPAT');
+	
+	// Translate arguments
+	constMappingTable[0]      = 'HTML_SPECIALCHARS';
+	constMappingTable[1]      = 'HTML_ENTITIES';
+	constMappingQuoteStyle[0] = 'ENT_NOQUOTES';
+	constMappingQuoteStyle[2] = 'ENT_COMPAT';
+	constMappingQuoteStyle[3] = 'ENT_QUOTES';
+	
+	// Map numbers to strings for compatibilty with PHP constants
+	if (!isNaN(useTable)) {
+			useTable = constMappingTable[useTable];
+	}
+	if (!isNaN(useQuoteStyle)) {
+			useQuoteStyle = constMappingQuoteStyle[useQuoteStyle];
+	}
+
+	if (useTable == 'HTML_SPECIALCHARS') {
+			// ascii decimals for better compatibility
+			entities['38'] = '&amp;';
+			if (useQuoteStyle != 'ENT_NOQUOTES') {
+					entities['34'] = '&quot;';
+			}
+			if (useQuoteStyle == 'ENT_QUOTES') {
+					entities['39'] = '&#039;';
+			}
+			entities['60'] = '&lt;';
+			entities['62'] = '&gt;';
+	} else if (useTable == 'HTML_ENTITIES') {
+			// ascii decimals for better compatibility
+		entities['38']  = '&amp;';
+			if (useQuoteStyle != 'ENT_NOQUOTES') {
+					entities['34'] = '&quot;';
+			}
+			if (useQuoteStyle == 'ENT_QUOTES') {
+					entities['39'] = '&#039;';
+			}
+		entities['60']  = '&lt;';
+		entities['62']  = '&gt;';
+		entities['160'] = '&nbsp;';
+		entities['161'] = '&iexcl;';
+		entities['162'] = '&cent;';
+		entities['163'] = '&pound;';
+		entities['164'] = '&curren;';
+		entities['165'] = '&yen;';
+		entities['166'] = '&brvbar;';
+		entities['167'] = '&sect;';
+		entities['168'] = '&uml;';
+		entities['169'] = '&copy;';
+		entities['170'] = '&ordf;';
+		entities['171'] = '&laquo;';
+		entities['172'] = '&not;';
+		entities['173'] = '&shy;';
+		entities['174'] = '&reg;';
+		entities['175'] = '&macr;';
+		entities['176'] = '&deg;';
+		entities['177'] = '&plusmn;';
+		entities['178'] = '&sup2;';
+		entities['179'] = '&sup3;';
+		entities['180'] = '&acute;';
+		entities['181'] = '&micro;';
+		entities['182'] = '&para;';
+		entities['183'] = '&middot;';
+		entities['184'] = '&cedil;';
+		entities['185'] = '&sup1;';
+		entities['186'] = '&ordm;';
+		entities['187'] = '&raquo;';
+		entities['188'] = '&frac14;';
+		entities['189'] = '&frac12;';
+		entities['190'] = '&frac34;';
+		entities['191'] = '&iquest;';
+		entities['192'] = '&Agrave;';
+		entities['193'] = '&Aacute;';
+		entities['194'] = '&Acirc;';
+		entities['195'] = '&Atilde;';
+		entities['196'] = '&Auml;';
+		entities['197'] = '&Aring;';
+		entities['198'] = '&AElig;';
+		entities['199'] = '&Ccedil;';
+		entities['200'] = '&Egrave;';
+		entities['201'] = '&Eacute;';
+		entities['202'] = '&Ecirc;';
+		entities['203'] = '&Euml;';
+		entities['204'] = '&Igrave;';
+		entities['205'] = '&Iacute;';
+		entities['206'] = '&Icirc;';
+		entities['207'] = '&Iuml;';
+		entities['208'] = '&ETH;';
+		entities['209'] = '&Ntilde;';
+		entities['210'] = '&Ograve;';
+		entities['211'] = '&Oacute;';
+		entities['212'] = '&Ocirc;';
+		entities['213'] = '&Otilde;';
+		entities['214'] = '&Ouml;';
+		entities['215'] = '&times;';
+		entities['216'] = '&Oslash;';
+		entities['217'] = '&Ugrave;';
+		entities['218'] = '&Uacute;';
+		entities['219'] = '&Ucirc;';
+		entities['220'] = '&Uuml;';
+		entities['221'] = '&Yacute;';
+		entities['222'] = '&THORN;';
+		entities['223'] = '&szlig;';
+		entities['224'] = '&agrave;';
+		entities['225'] = '&aacute;';
+		entities['226'] = '&acirc;';
+		entities['227'] = '&atilde;';
+		entities['228'] = '&auml;';
+		entities['229'] = '&aring;';
+		entities['230'] = '&aelig;';
+		entities['231'] = '&ccedil;';
+		entities['232'] = '&egrave;';
+		entities['233'] = '&eacute;';
+		entities['234'] = '&ecirc;';
+		entities['235'] = '&euml;';
+		entities['236'] = '&igrave;';
+		entities['237'] = '&iacute;';
+		entities['238'] = '&icirc;';
+		entities['239'] = '&iuml;';
+		entities['240'] = '&eth;';
+		entities['241'] = '&ntilde;';
+		entities['242'] = '&ograve;';
+		entities['243'] = '&oacute;';
+		entities['244'] = '&ocirc;';
+		entities['245'] = '&otilde;';
+		entities['246'] = '&ouml;';
+		entities['247'] = '&divide;';
+		entities['248'] = '&oslash;';
+		entities['249'] = '&ugrave;';
+		entities['250'] = '&uacute;';
+		entities['251'] = '&ucirc;';
+		entities['252'] = '&uuml;';
+		entities['253'] = '&yacute;';
+		entities['254'] = '&thorn;';
+		entities['255'] = '&yuml;';
+	} else {
+			throw Error("Table: "+useTable+' not supported');
+			return false;
+	}
+	
+	// ascii decimals to real symbols
+	for (decimal in entities) {
+			symbol = String.fromCharCode(decimal);
+			histogram[symbol] = entities[decimal];
+	}
+	
+	return histogram;
+});
+
+JSON=function(){function f(n){return n<10?'0'+n:n;}Date.prototype.toJSON=function(key){return this.getUTCFullYear()+'-'+f(this.getUTCMonth()+1)+'-'+f(this.getUTCDate())+'T'+f(this.getUTCHours())+':'+f(this.getUTCMinutes())+':'+f(this.getUTCSeconds())+'Z';};var cx=/[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,escapeable=/[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,gap,indent,meta={'\b':'\\b','\t':'\\t','\n':'\\n','\f':'\\f','\r':'\\r','"':'\\"','\\':'\\\\'},rep;function quote(string){escapeable.lastIndex=0;return escapeable.test(string)?'"'+string.replace(escapeable,function(a){var c=meta[a];if(typeof c==='string'){return c;}return'\\u'+('0000'+(+(a.charCodeAt(0))).toString(16)).slice(-4);})+'"':'"'+string+'"';}function str(key,holder){var i,k,v,length,mind=gap,partial,value=holder[key];if(value&&typeof value==='object'&&typeof value.toJSON==='function'){value=value.toJSON(key);}if(typeof rep==='function'){value=rep.call(holder,key,value);}switch(typeof value){case'string':return quote(value);case'number':return isFinite(value)?String(value):'null';case'boolean':case'null':return String(value);case'object':if(!value){return'null';}gap+=indent;partial=[];if(typeof value.length==='number'&&!(value.propertyIsEnumerable('length'))){length=value.length;for(i=0;i<length;i+=1){partial[i]=str(i,value)||'null';}v=partial.length===0?'[]':gap?'[\n'+gap+partial.join(',\n'+gap)+'\n'+mind+']':'['+partial.join(',')+']';gap=mind;return v;}if(rep&&typeof rep==='object'){length=rep.length;for(i=0;i<length;i+=1){k=rep[i];if(typeof k==='string'){v=str(k,value,rep);if(v){partial.push(quote(k)+(gap?': ':':')+v);}}}}else{for(k in value){if(Object.hasOwnProperty.call(value,k)){v=str(k,value,rep);if(v){partial.push(quote(k)+(gap?': ':':')+v);}}}}v=partial.length===0?'{}':gap?'{\n'+gap+partial.join(',\n'+gap)+'\n'+mind+'}':'{'+partial.join(',')+'}';gap=mind;return v;}}return{stringify:function(value,replacer,space){var i;gap='';indent='';if(typeof space==='number'){for(i=0;i<space;i+=1){indent+=' ';}}else if(typeof space==='string'){indent=space;}rep=replacer;if(replacer&&typeof replacer!=='function'&&(typeof replacer!=='object'||typeof replacer.length!=='number')){throw new Error('JSON.stringify');}return str('',{'':value});},parse:function(text,reviver){var j;function walk(holder,key){var k,v,value=holder[key];if(value&&typeof value==='object'){for(k in value){if(Object.hasOwnProperty.call(value,k)){v=walk(value,k);if(v!==undefined){value[k]=v;}else{delete value[k];}}}}return reviver.call(holder,key,value);}cx.lastIndex=0;if(cx.test(text)){text=text.replace(cx,function(a){return'\\u'+('0000'+(+(a.charCodeAt(0))).toString(16)).slice(-4);});}if(/^[\],:{}\s]*$/.test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g,'@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,']').replace(/(?:^|:|,)(?:\s*\[)+/g,''))){j=eval('('+text+')');return typeof reviver==='function'?walk({'':j},''):j;}throw new SyntaxError('JSON.parse');}};}();
+
 elation.extend('JSON', new function() {
   this.parse = function(text) {
     return this.JSON(['decode','parse'],text);
@@ -551,7 +725,7 @@ elation.extend('JSON', new function() {
 			? parms[0]
 			: parms[1];
     
-		return JSON[key](text);
+		return (key == 'parse' ? JSON.parse(text) : JSON.stringify(text));
   }
 });
 elation.extend('cookie', {

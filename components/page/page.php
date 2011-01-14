24f39791edc1bf5a27237cb5566101edcadbadfa
lucian303 <lucian303@gmail.com>
2011-01-14 12:51:59 -0800

    - Added page.sizelog controller. Doesn't seem to be called from any js yet.

--- components/page/page.php
+++ components/page/page.php
@@ -1,11 +1,31 @@
-<?
+<?php
+class Component_page extends Component
+{
+  public function init() {}
 
-class Component_page extends Component {
-  function init() {
-  }
-
-  function controller_page($args) {
+  public function controller_page($args)
+  {
     $vars["args"] = $args;
     return $this->GetComponentResponse("./page.tpl", $vars);
+  }
+
+  public function component_sizelog($args)
+  {
+    $pandora = PandoraLog::singleton();
+    $session = SessionManager::singleton();
+    
+    $size = array();
+    $size["timestamp"]            = time();
+    $size["session_id"]           = $session->flsid;
+    $size["fluid"]                = $session->fluid;
+    $size["version"]              = $this->root->version;
+    $size["width"]                = $args["width"];
+    $size["height"]               = $args["height"];
+    $size["result_view_id"]       = $args["result_view_id"];
+
+    // add data
+    $pandora->addData("sizes", $size);
+    unset($size);
+    return '';
   }
 }  

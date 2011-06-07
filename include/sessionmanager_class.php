a3bbb63993b33bc2a767881222150737bbbb0af4
James Baicoianu <james@thefind.com>
2011-06-06 23:11:36 -0700

    - Don't attempt to start session if headers are already sent - Modified SessionManager to use DataManager properly

--- include/sessionmanager_class.php
+++ include/sessionmanager_class.php
@@ -108,10 +108,10 @@ class SessionManager
     global $webapp;
 
     $this->data = DataManager::singleton();
-    //Profiler::StartTimer("SessionManager::Init()");
+    Profiler::StartTimer("SessionManager::Init()", 2);
 
+    /*
     if ($this->data->caches["memcache"]["session"] !== NULL) {
-      $cfgmgr = ConfigManager::singleton();
       $this->cache_obj = $this->data->caches["memcache"]["session"];
       //$this->session_cache_expire = $this->data->caches["memcache"]["session"]->lifetime;
     } else {
@@ -121,6 +121,7 @@ class SessionManager
       Logger::Error("SessionManager::init() - Cannot connect to session memcache - " . $this->data->sources);
       $this->cache_obj =& NoCache::singleton();
     }
+    */
 
     // instantiate the pandora object
     $pandora = PandoraLog::singleton();
@@ -171,7 +172,9 @@ class SessionManager
     session_cache_limiter('private');
 
     // initiate sessionization
-    session_start();
+    if (!headers_sent()) {
+      session_start();
+    }
 
     /**
      * Read the permanent session ID from cookie.
@@ -226,7 +229,9 @@ class SessionManager
         $this->first_session_for_day = 1;
       }
       $fluid_data = $this->fluid.','.$this->session_count.','.$this->last_access;
-      setcookie('fl-uid', $fluid_data, time()+31536000, "/", $domain); // (update the permanent cookie)
+      if (!headers_sent()) {
+        setcookie('fl-uid', $fluid_data, time()+31536000, "/", $domain); // (update the permanent cookie)
+      }
     }
 
 
@@ -241,7 +246,8 @@ class SessionManager
      * If not in the DB, create a new session.
      */
     $this->flsid = session_id();
-    $session_memcache = $this->cache_obj->get($this->flsid);
+    //$session_memcache = $this->cache_obj->get($this->flsid);
+    $session_memcache = DataManager::fetch("memcache.session#{$this->flsid}", $this->flsid);
     //$tmpsession = unserialize($session_memcache);
 
     if (!empty($session_memcache["fluid"]))
@@ -334,7 +340,7 @@ class SessionManager
     // set the script session ID
     $this->flssid = $this->generate_fluid();
 
-    //Profiler::StopTimer("SessionManager::Init()");
+    Profiler::StopTimer("SessionManager::Init()");
   }
   public function quit() {
     // instantiate the pandora object
@@ -415,7 +421,8 @@ class SessionManager
   public function read($id)
   {
     // calculate the crc value of this for comparison later
-    $cacheentry = $this->cache_obj->get($id);
+    //$cacheentry = $this->cache_obj->get($id);
+    $cacheentry = DataManager::fetch("memcache.session#{$id}", $id);
     if ($cacheentry instanceOf CacheEntry)
       $data = $cacheentry->getPayload();
     else
@@ -450,13 +457,16 @@ class SessionManager
     $pdata = $_SESSION["persist"];
     $pdata_serialize = serialize($pdata);
 
-    $data = $this->cache_obj->get($id);
+    //Logger::Warn($pdata_serialize);
+    //$data = $this->cache_obj->get($id);
+    $data = DataManager::fetch("memcache.session#{$id}", $id);
     $pdata_before = $data["persist"];
     $pdata_before_serialize = serialize($pdata_before);
 
     // update the memcache with the entire $_SESSION
     //$session_serialize = serialize($_SESSION);
-    $this->cache_obj->set($id, $_SESSION, 0);
+    //$this->cache_obj->set($id, $_SESSION, 0);
+    $data = DataManager::update("memcache.session#{$id}", $id, $_SESSION);
 
     if (empty($pdata) || is_null($pdata)) {
       $new_crc = 0;
@@ -471,7 +481,8 @@ class SessionManager
         $this->create_new_persist_record();
         // need to set the session cache again with we set the has_db_record param
         //$session_serialize = serialize($_SESSION);
-        $this->cache_obj->set($id, $_SESSION, 0, $this->session_cache_expire);
+        //$this->cache_obj->set($id, $_SESSION, 0, $this->session_cache_expire);
+        DataManager::update("memcache.session#{$id}", $id, $_SESSION);
       } else {
         /*
         $result = $this->data->Query("db.userdata.usersession.{$id}:nocache",

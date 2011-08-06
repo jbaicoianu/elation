e9d4b6dd7f32d474d46fb3c362dd5173277a449b
James Baicoianu <james_github@baicoianu.com>
2011-08-06 04:36:44 -0700

    Basic improvements to User dummy class

--- include/user_class.php
+++ include/user_class.php
@@ -1,7 +1,10 @@
 <?
+// FIXME/TODO - this is a dummy class, and does not actually implement any of 
+//              the methods necessary to represent a logged-in user.
+
 class User {
   protected static $instance = NULL;
-  public static $usertypes = array();
+  public static $usertypes = array("anonymous");
 
   public function InitActiveUser($req) {
   }
@@ -9,14 +12,19 @@ class User {
     return false;
   }
   public function HasRole($role) {
+    return true; // FIXME - hardcoded to true, since this is just a dummy class right now...
     $roles = $this->GetRoles();
     return (!empty($roles) && isset($roles[$role]));
   } 
   public function GetRoles() {
     return array();
   }
-
   public function save() {
+  }
+
+  public static function authorized($role) {
+    $user = self::singleton();
+    return $user->HasRole($role);
   }
 
   public static function singleton() {

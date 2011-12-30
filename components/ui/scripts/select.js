9f0a810b3e5eb64995272b263da98a85dd2c7251
James Baicoianu <james_github@baicoianu.com>
2011-12-30 03:34:13 -0800

    Filled in more functionality for ui.select

--- components/ui/scripts/select.js
+++ components/ui/scripts/select.js
@@ -1,12 +1,24 @@
 elation.component.add('ui.select', {
-  init: function(name, container, args) {
+  init: function() {
+    elation.events.add(this.container, "change", this);
   },
   setItems: function(items) {
-    if (typeof items == 'array') {
+    if (items instanceof Array) {
       this.set('args.items', items.join(';'));
     } else {
       this.set('args.items', items);
+      items = items.split(';');
     }
+    this.container.innerHTML = '';
+    for (var i = 0; i < items.length; i++) {
+      var option = elation.html.create({tag: 'option'});
+      option.value = items[i];
+      option.innerHTML = items[i];
+      this.container.appendChild(option);
+    }
+  },
+  change: function(ev) {
+    elation.events.fire({type: "ui_select_change", data: this.container.value, element: this});
   }
 });
 

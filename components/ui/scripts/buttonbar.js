f1425e8d0d29c7ac682d53941c00d26d1bf61c91
James Baicoianu <james_github@baicoianu.com>
2013-08-26 01:31:21 -0700

    Removed debug info

--- components/ui/scripts/buttonbar.js
+++ components/ui/scripts/buttonbar.js
@@ -5,12 +5,12 @@ elation.component.add('ui.buttonbar', function() {
       'buttons': { type: 'list' }
     });
     */
-console.log('new buttonbar', this.args.buttons);
+    //console.log('new buttonbar', this.args.buttons);
     if (this.args.buttons) {
       for (var i in this.args.buttons) {
         var buttonargs = this.args.buttons[i];
         var button = elation.ui.button(null, elation.html.create({tag: 'button', append: this.container}), buttonargs, buttonargs.events);
-console.log('\t-', button);
+        //console.log('\t-', button);
       }
     }
   }

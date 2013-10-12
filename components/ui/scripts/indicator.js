2b84c70b57b2043b2b895015ef2de086312a8f73
James Baicoianu <james_github@baicoianu.com>
2013-10-12 12:27:58 -0700

    ui.indicator tweaks

--- components/ui/scripts/indicator.js
+++ components/ui/scripts/indicator.js
@@ -1,10 +1,21 @@
 elation.component.add('ui.indicator', function() {
   this.init = function() {
     elation.html.addclass(this.container, 'ui_indicator');
+    elation.events.add(this.container, 'click', this);
   }
   this.setState = function(state) {
+    if (this.state && this.state != state) {
+      this.removeState(this.state);
+    }
+    this.state = state;
     if (!elation.html.hasclass(this.container, state)) {
       elation.html.addclass(this.container, state);
     }
+  }
+  this.removeState = function(state) {
+    elation.html.removeclass(this.container, state);
+  }
+  this.click = function(ev) {
+    elation.events.fire({type: 'click', element: this, event: ev});
   }
 });

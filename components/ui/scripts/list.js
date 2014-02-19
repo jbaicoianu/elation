f5d414e6c63032e2303bb248022a4b0be6917688
James Baicoianu <james_github@baicoianu.com>
2014-02-19 00:11:53 -0800

    Significant improvements to ui.list (itemcontainer support, better event handling, smart rendering, documentation)

--- components/ui/scripts/list.js
+++ components/ui/scripts/list.js
@@ -1,4 +1,26 @@
+/** 
+ * List UI element
+ *
+ * @class list
+ * @augments elation.ui.base
+ * @memberof elation.ui
+ * @alias elation.ui.list
+ *
+ * @param {object} args
+ * @param {string} args.tag
+ * @param {string} args.classname
+ * @param {string} args.title
+ * @param {boolean} args.draggable
+ * @param {boolean} args.hidden
+ * @param {string} args.orientation
+ * @param {string} args.sortbydefault
+ * @param {array} args.items
+ * @param {elation.container.simple} args.itemcontainer
+ * @param {object} args.attrs
+ */
 elation.component.add('ui.list', function() {
+  this.defaultcontainer = {tag: 'ul', classname: 'ui_list'};
+
   this.init = function(name, container, args) {
     this.tag = this.args.tag || this.container.tagName || 'DIV';
     this.classname = this.args.classname || "";
@@ -9,25 +31,61 @@ elation.component.add('ui.list', function() {
     this.items = [];
     this.listitems = [];
 
+    this.renderLoopActive = false;
+    this.dirty = false;
+
     this.animatetime = 850;
 
-    elation.html.addclass(this.container, 'ui_list');
     this.setOrientation(this.orientation);
 
     if (this.args.sortbydefault) {
       this.setSortBy(this.args.sortbydefault);
     }
-
-    this.setItems(this.args.items);
+    if (this.args.hidden) {
+      this.hide();
+    }
+    if (this.args.itemcontainer) {
+      this.setItemContainer(this.args.itemcontainer);
+    } else if (this.args.items) {
+      this.setItems(this.args.items);
+    } else {
+      this.extractItems();
+    }
   }
+  /**
+   * Returns the UL element for this component, or create a new one if it doesn't exist yet
+   * @function getListElement
+   * @memberof elation.ui.list#
+   * @returns {HTMLUListElement}
+   */
+  this.getListElement = function() {
+    if (this.container instanceof HTMLUListElement) {
+      return this.container;
+    } else if (!this.listul) {
+      this.listul = elation.html.create({tag: 'ul', append: this.container});
+    }
+    return this.listul;
+  }
+  /**
+   * Combine passed-in attributes with built-in defaults
+   * @function getDefaultAttributes
+   * @memberof elation.ui.list#
+   * @returns {Object}
+   */
   this.getDefaultAttributes = function() {
     var attrs = this.args.attrs || {};
     if (elation.utils.isEmpty(attrs.name)) attrs.name = 'name';
     if (elation.utils.isEmpty(attrs.children)) attrs.children = 'items';
+    if (elation.utils.isEmpty(attrs.label)) attrs.label = 'label';
     return attrs;
   }
+  /**
+   * Update the items associated with this list
+   * @function setItems
+   * @memberof elation.ui.list#
+   */
   this.setItems = function(items) {
-    this.clear();
+    //this.clear();
     if (elation.utils.isArray(items)) {
       this.items = items;
     } else {
@@ -37,53 +95,158 @@ elation.component.add('ui.list', function() {
     }
     this.refresh();
   }
-  this.setOrientation = function(orientation) {
-    if (this.orientation) {
-      elation.html.removeclass(this.container, 'orientation_' + this.orientation);
+  /**
+   * Links this list component with a container to automatically handle updates when data changes
+   * @function setItemContainer
+   * @memberof elation.ui.list#
+   * @param {elation.collection.simple} itemcontainer  
+   */
+  this.setItemContainer = function(itemcontainer) {
+    if (this.itemcontainer) {
+      elation.events.remove(this.itemcontainer, "container_add,container_remove,container_move", this);
     }
-    this.orientation = orientation;
-    elation.html.addclass(this.container, 'orientation_' + this.orientation);
+    this.itemcontainer = itemcontainer;
+    this.setItems(this.itemcontainer.items);
+    elation.events.add(this.itemcontainer, "container_add,container_remove,container_move", this);
   }
+  /**
+   * Extracts items out of the list's existing HTML structure
+   * @function extractItems
+   * @memberof elation.ui.list#
+   */
+  this.extractItems = function() {
+    var items = [];
+    var attrs = this.getDefaultAttributes();
+    for (var i = 0; i < this.container.childNodes.length; i++) {
+      var node = this.container.childNodes[i];
+      if (node instanceof HTMLLIElement) {
+        items.push({label: node.innerText});
+      }
+    }
+    this.setItems(items);
+  }
+  /**
+   * Add a new item to this list
+   * @function addItem
+   * @memberof elation.ui.list#
+   * @param {Object} item
+   */
   this.addItem = function(item) {
     this.items.push(item);
     this.refresh();
   }
+  /**
+   * Add a new item to a specific position in this list
+   * @function addItemAtPosition
+   * @memberof elation.ui.list#
+   * @param {Object} item
+   * @param {integer} position
+   */
   this.addItemAtPosition = function(item, position) {
     this.items.splice(position, 0, item);
     this.listitems.splice(position, 0, null);
     this.refresh();
   }
+  /**
+   * Resets the list to empty
+   * @function clear
+   * @memberof elation.ui.list#
+   */
   this.clear = function() {
+    var ul = this.getListElement();
     for (var i = 0; i < this.items.length; i++) {
       if (this.listitems[i]) {
-        this.listul.removeChild(this.listitems[i].container);
+        ul.removeChild(this.listitems[i].container);
         delete this.listitems[i];
         delete this.items[i];
       }
     }
     this.listitems = [];
     this.items = [];
+    ul.innerHTML = '';
   }
-  this.refresh = function() {
-    if (!this.listul) {
-      this.listul = elation.html.create({tag: 'ul', append: this.container});
-    }
+  /**
+   * Get the elation.ui.listitem for a specified item, allocating as needed
+   * @function getlistitem
+   * @memberof elation.ui.list#
+   * @param {Object} item
+   * @returns {elation.ui.listitem}
+   */
+  this.getlistitem = function(item) {
     var attrs = this.getDefaultAttributes();
-    for (var i = 0; i < this.items.length; i++) {
-      if (!this.listitems[i]) {
-        var li = elation.html.create({tag: 'li'});
-        if (this.listitems[i+1]) {
-          this.listul.insertBefore(li, this.listitems[i+1].container);
-        } else {
-          this.listul.appendChild(li);
-        }
-        this.listitems[i] = elation.ui.listitem(null, li, {item: this.items[i], attrs: attrs});
+    for (var i = 0; i < this.listitems.length; i++) {
+      if (this.listitems[i].value === item) {
+        return this.listitems[i];
       }
     }
-    elation.component.init();
+    
+    // no existing listitem, allocate a new one
+    var newlistitem = elation.ui.listitem(null, elation.html.create({tag: 'li'}), {item: item, attrs: attrs});
+    this.listitems.push(newlistitem);
+    elation.events.add(newlistitem, 'ui_list_item_select', this);
+    return newlistitem;
   }
+  /**
+   * Mark data as dirty, and then start the animation loop if not already active
+   * @function refresh
+   * @memberof elation.ui.list#
+   */
+  this.refresh = function() {
+    this.dirty = true;
+    if (!this.renderLoopActive) {
+      this.render(true);
+    }
+  }
+
+  /**
+   * Updates the listitem objects and the HTML representation of this list with any new or removed items
+   * @function render
+   * @memberof elation.ui.list#
+   */
+  this.render = function() {
+    if (this.dirty) {
+      var ul = this.getListElement();
+
+      // FIXME - this could be made more efficient in two ways:
+      //   1) instead of removing all elements and then re-adding them in order, we should be
+      //      able to figure out deletions, additions, and moves and apply them separately
+      //   2) currently when we remove listitems, we still keep a reference to the old object which gets
+      //      reused if the same item is re-added.  this can be a performance optimization in some
+      //      cases (automatic object reuse reduces gc if the same objects are added and removed repeatedly
+      //      over the lifetime of the list), but can be a memory leak in cases where lots of 
+      //      non-repeating data is added and removed.
+
+      for (var i = 0; i < this.listitems.length; i++) {
+        if (this.listitems[i].container.parentNode == ul) {
+          ul.removeChild(this.listitems[i].container); 
+        }
+      }
+      for (var i = 0; i < this.items.length; i++) {
+        var listitem = this.getlistitem(this.items[i]);
+        if (listitem.container.parentNode != ul) {
+          ul.appendChild(listitem.container);
+        }
+      }
+      elation.component.init();
+
+      this.dirty = false;
+      requestAnimationFrame(elation.bind(this, this.render));
+      this.renderLoopActive = true;
+    } else if (!this.dirty) {
+      this.renderLoopActive = false;
+    } 
+  }
+
+  /**
+   * Sorts the items in the list by the specified key
+   * @function sort
+   * @memberof elation.ui.list#
+   * @param {string} sortby
+   * @param {boolean} reverse
+   */
   this.sort = function(sortby, reverse) {
     if (!reverse) reverse = false; // force to bool
+    var ul = this.getListElement();
 
     // Resort list items
     // FIXME - should also update this.items to reflect new order
@@ -107,8 +270,8 @@ elation.component.add('ui.list', function() {
     // FIXME - this could be much more efficient, and is probably the slowest part of the whole process
     for (var i = 0; i < items.length; i++) {
       elation.html.removeclass(items[i].container, 'state_animating');
-      this.listul.removeChild(items[i].container);
-      this.listul.appendChild(items[i].container);
+      ul.removeChild(items[i].container);
+      ul.appendChild(items[i].container);
     }
     // Calculate new item positions, and set transform
     var maxdist = 0;
@@ -143,6 +306,12 @@ elation.component.add('ui.list', function() {
     // Set classname based on sortby parameter
     this.setSortBy(sortby);
   }
+  /**
+   * Sets the current sorting mode for this class
+   * @function setSortBy
+   * @memberof elation.ui.list#
+   * @param {string} sortby
+   */
   this.setSortBy = function(sortby) {
     if (this.sortby) {
       elation.html.removeclass(this.container, 'ui_list_sortby_' + this.sortby);
@@ -150,29 +319,127 @@ elation.component.add('ui.list', function() {
     this.sortby = sortby;
     elation.html.addclass(this.container, 'ui_list_sortby_' + this.sortby);
   }
-});
+  /**
+   * Event handler: elation.ui.listitem#ui_list_item_select
+   * @function ui_list_item_select
+   * @memberof elation.ui.list#
+   * @param {event} ev
+   */
+  this.ui_list_item_select = function(ev) {
+    if (this.selecteditem) {
+      this.selecteditem.unselect();
+    }
+    this.selecteditem = ev.target;
+    elation.events.fire({type: 'ui_list_select', element: this, data: ev.data});
+  }
+  /**
+   * Event handler: elation.container.simple#container_add
+   * @function container_add
+   * @memberof elation.ui.list#
+   * @param {event} ev
+   */
+  this.container_add = function(ev) {
+    this.refresh();
+  }
+  /**
+   * Event handler: elation.container.simple#container_remove
+   * @function container_remove
+   * @memberof elation.ui.list#
+   * @param {event} ev
+   */
+  this.container_remove = function(ev) {
+    this.refresh();
+  }
+  /**
+   * Event handler: elation.container.simple#container_move
+   * @function container_move
+   * @memberof elation.ui.list#
+   * @param {event} ev
+   */
+  this.container_move = function(ev) {
+    this.refresh();
+  }
+}, elation.ui.base);
+
+/** 
+ * ListItem UI element
+ * Represents an individual item in a ui.list
+ *
+ * @class listitem
+ * @augments elation.ui.base
+ * @memberof elation.ui
+ * @alias elation.ui.listitem
+ *
+ * @param {object} args
+ * @param {object} args.item
+ * @param {object} args.attrs
+ */
 elation.component.add('ui.listitem', function() {
   this.init = function() {
     this.value = this.args.item;
     this.attrs = this.args.attrs || {};
-    if (!this.attrs.label) this.attrs.label = 'label';
 
-    if (this.value.classname) {
-      elation.html.addclass(this.container, this.value.classname);
-    }
-
+    elation.html.addclass(this.container, 'ui_list_item');
     if (this.value) {
+      if (this.value.classname) {
+        elation.html.addclass(this.container, this.value.classname);
+      }
+
+      var filled = false;
       if (this.value instanceof elation.component.base) {
         this.container.appendChild(this.value.container);
+        filled = true;
       } else if (this.attrs.itemtemplate) {
         this.container.innerHTML = elation.template.get(this.attrs.itemtemplate, this.value);
-      } else if (this.value[this.attrs.label]) {
-        this.container.innerHTML = this.value[this.attrs.label];
+        filled = true;
+      } else if (this.attrs.itemcomponent) {
+        var itemcomponentclass = elation.utils.arrayget(elation, this.attrs.itemcomponent);
+        if (itemcomponentclass) {
+          var itemcomponent = itemcomponentclass(null, this.container, this.value);
+          filled = true;
+        }
+      } 
+      if (!filled) {
+        if (elation.utils.isString(this.value)) {
+          this.container.innerHTML = this.value;
+        } else if (this.value[this.attrs.label]) {
+          this.container.innerHTML = this.value[this.attrs.label];
+        }
       }
 
       if (!elation.utils.isEmpty(this.attrs.disabled) && !elation.utils.isEmpty(this.value[this.attrs.disabled])) {
         elation.html.addclass(this.container, "state_disabled");
       }
     }
+    elation.events.add(this.container, 'click', this);
+  }
+  /**
+   * Set this list item as being selected
+   * @function select
+   * @memberof elation.ui.listitem#
+   * @emits elation.ui.listitem#ui_list_item_select
+   */
+  this.select = function() {
+    elation.html.addclass(this.container, 'state_selected');
+    elation.events.fire({type: 'ui_list_item_select', element: this, data: this.value});
+  }
+  /**
+   * Set this list item as being unselected
+   * @function unselect
+   * @memberof elation.ui.listitem#
+   * @emits elation.ui.listitem#ui_list_item_unselect
+   */
+  this.unselect = function() {
+    elation.html.removeclass(this.container, 'state_selected');
+    elation.events.fire({type: 'ui_list_item_unselect', element: this, data: this.value});
+  }
+  /**
+   * Event handler: HTML element click
+   * @function click
+   * @memberof elation.ui.listitem#
+   * @param {event} ev
+   */
+  this.click = function(ev) {
+    this.select();
   }
 });
--- components/ui/templates/list.tpl
+++ components/ui/templates/list.tpl
@@ -1,4 +1,5 @@
 {component name="elation.component" component="ui.list" tag=$tag id=$id classname=$class events=$events tagtype="open"}
+  {if !empty($itemcomponent)}<data class="elation-args" name="attrs.itemcomponent">{$itemcomponent}</data>{/if}
  {foreach from=$listitems item=itemchunk}
   <li>  
    {foreach from=$itemchunk key=k item=i}{component name=$itemcomponent item=$i itemname=$k}{/foreach}

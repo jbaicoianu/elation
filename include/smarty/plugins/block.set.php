3793932a85ff96659e6a15b3938ce73f80d7bf0f
James Baicoianu <james_github@baicoianu.com>
2014-03-30 13:35:39 -0700

    Fix for problems with some smarty installs

--- include/smarty/plugins/block.set.php
+++ include/smarty/plugins/block.set.php
@@ -3,7 +3,7 @@ function smarty_block_set($params, $content, &$smarty) {
   if (!empty($params["var"])) {
     $mode = any($params["mode"], "set");
 
-    $realsmarty = ($smarty instanceof Smarty_Internal_Template ? $smarty->parent : $smarty);
+    $realsmarty = ($smarty instanceof Smarty_Internal_Template || $smarty instanceof TemplateManager ? $smarty->parent : $smarty);
 
     if ($mode == "set")
       $realsmarty->varreplace[$params["var"]] = $content;

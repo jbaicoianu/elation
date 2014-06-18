f8848d63c11b28a8e8c8abbf4bbb3e7f6ef6d811
James Lazarus <jmslazarus@yahoo.com>
2014-06-18 15:25:45 -0700

    test

--- components/utils/htdocs/scripts/panel.js
+++ components/utils/htdocs/scripts/panel.js
@@ -350,7 +350,7 @@ elation.extend('panel.obj', function(name, parent, args) {
     this.loadtab_ajaxparms = parms;
 		this.loadtab_status = 'fetching_ajax';
 
-		var ajax = new elation.server();
+		//var ajax = new elation.server();
 
 		// ajax-fetch tab content, here we go!
 		ajaxlib.Queue({

9b2da6a7893018225120cd2d6d3b6a8254cbbc40
James Baicoianu <james_github@baicoianu.com>
2014-08-07 20:44:43 -0700

    Updated ui.buttonbar to use elation.require and ui.base

--- components/ui/scripts/buttonbar.js
+++ components/ui/scripts/buttonbar.js
@@ -8,25 +8,41 @@
  * @param {object} args
  * @param {object} args.buttons
  */
-elation.component.add('ui.buttonbar', function() {
-  this.init = function() {
-    /*
-    this.defineProperties({
-      'buttons': { type: 'list' }
-    });
-    */
-    //console.log('new buttonbar', this.args.buttons);
-    elation.html.addclass(this.container, 'ui_buttonbar');
-    this.buttons = {};
-    if (this.args.buttons) {
-      for (var i in this.args.buttons) {
-        var buttonargs = this.args.buttons[i];
-        //var button = elation.ui.button(null, elation.html.create({tag: 'button', append: this.container}), buttonargs, buttonargs.events);
-        var button = elation.ui.button(buttonargs);
-        button.reparent(this.container);
-        this.buttons[i] = button;
-        //console.log('\t-', button);
+elation.require(['ui.base', 'ui.button'], function() {
+  elation.component.add('ui.buttonbar', function() {
+    this.defaultcontainer = { tag: 'div', classname: 'ui_buttonbar' };
+    this.init = function() {
+      elation.ui.buttonbar.extendclass.init.call(this);
+      if (this.args.classname) {
+        this.addclass(this.args.classname);
+      }
+      /*
+      this.defineProperties({
+        'buttons': { type: 'list' }
+      });
+      */
+      //console.log('new buttonbar', this.args.buttons);
+      this.buttons = {};
+      if (this.args.buttons) {
+        for (var i in this.args.buttons) {
+          var buttonargs = this.args.buttons[i];
+          //var button = elation.ui.button(null, elation.html.create({tag: 'button', append: this.container}), buttonargs, buttonargs.events);
+          var button = (buttonargs.toggle ? elation.ui.togglebutton(buttonargs) : elation.ui.button(buttonargs));
+          button.reparent(this.container);
+          this.buttons[i] = button;
+          //console.log('\t-', button);
+        }
       }
     }
-  }
-}, elation.ui.base);
+    this.enable = function() {
+      for (var k in this.buttons) {
+        this.buttons[k].disabled = false;
+      }
+    }
+    this.disable = function() {
+      for (var k in this.buttons) {
+        this.buttons[k].disabled = true;
+      }
+    }
+  }, elation.ui.base);
+});

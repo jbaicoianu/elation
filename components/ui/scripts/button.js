fffdef87ef2f3b781759de97afd903894aff499d
James Baicoianu <james_github@baicoianu.com>
2014-08-07 20:44:09 -0700

    Updated ui.button to use elation.require() and inherit from ui.base

--- components/ui/scripts/button.js
+++ components/ui/scripts/button.js
@@ -1,110 +1,169 @@
-/** 
- * Button UI element
- *
- * @class button
- * @augments elation.ui.base
- * @memberof elation.ui
- *
- * @param {object} args
- * @param {string} args.tag
- * @param {string} args.classname
- * @param {string} args.label
- * @param {string} args.title
- * @param {boolean} args.draggable
- * @param {boolean} args.autoblur
- * @param {boolean} args.autofocus
- */
-elation.component.add('ui.button', function() {
-  this.defaultcontainer = { tag: 'button', classname: 'ui_button' };
+elation.require(['ui.base'], function() {
+  /** 
+   * Button UI element
+   *
+   * @class button
+   * @augments elation.ui.base
+   * @memberof elation.ui
+   *
+   * @param {object} args
+   * @param {string} args.tag
+   * @param {string} args.classname
+   * @param {string} args.label
+   * @param {string} args.title
+   * @param {boolean} args.draggable
+   * @param {boolean} args.autoblur
+   * @param {boolean} args.autofocus
+   */
+  elation.component.add('ui.button', function() {
+    this.defaultcontainer = { tag: 'button', classname: 'ui_button' };
 
-  this.init = function(name, container, args) {
-    this.tag = this.args.tag || "BUTTON";
-    this.classname = this.args.classname || "";
-    this.title = this.args.title || false;
-    this.draggable = this.args.draggable || false;
-    this.label = this.args.label || container.innerHTML;
-    this.autoblur = this.args.autoblur || false;
+    this.init = function(name, container, args) {
+      this.tag = this.args.tag || "BUTTON";
+      this.classname = this.args.classname || "";
+      this.title = this.args.title || false;
+      this.draggable = this.args.draggable || false;
+      this.label = this.args.label || this.container.innerHTML;
+      this.autoblur = this.args.autoblur || false;
+      this.tabindex = this.args.tabindex || false;
 
-    elation.html.addclass(this.container, 'ui_button');
-    this.create();
-    elation.events.add(this.container, 'click', this);
+      this.create();
+      elation.events.add(this.container, 'click', this);
 
-    for (var k in this.events) {
-      elation.events.add(this.container, k, this.events[k]);
+      for (var k in this.events) {
+        elation.events.add(this.container, k, this.events[k]);
+      }
     }
-  }
-  /**
-   * Initialize HTML element
-   * @function create
-   * @memberof elation.ui.button#
-   */
-  this.create = function() {
-    //this.element = document.createElement(this.tag);
-    this.container.innerHTML = this.label;
-    var classname = '';
-    if (this.draggable) {
-      classname = 'elation_ui_button_draggable';
-      this.container.draggable = true;
+    /**
+     * Initialize HTML element
+     * @function create
+     * @memberof elation.ui.button#
+     */
+    this.create = function() {
+      //this.element = document.createElement(this.tag);
+      this.container.innerHTML = this.label;
+      var classname = 'ui_button ';
+      if (this.draggable) {
+        classname = 'elation_ui_button_draggable';
+        this.container.draggable = true;
+      }
+      classname += this.classname;
+      if (classname.length > 0) {
+        this.container.className = classname;
+      }
+      if (this.title)
+        this.container.title = this.title;
+      if (this.tabindex !== false) {
+        this.container.tabIndex = this.tabindex;
+      }
+      this.addPropertyProxies(['disabled']);
+      if (this.args.disabled) {
+        this.disabled = true;
+      }
     }
-    classname += this.classname;
-    if (classname.length > 0) {
-      this.container.className = classname;
+    /**
+     * Add as a child of the specified element, removing from current parent if necessary
+     * @function addTo
+     * @memberof elation.ui.button#
+     * @returns {boolean}
+     */
+    this.addTo = function(parent) {
+      if (typeof parent != 'undefined') {
+        if (!this.container)
+          this.create();
+        parent.appendChild(this.container);
+        return true;
+      }
+      return false;
     }
-    if (this.title)
-      this.container.title = this.title;
-  }
-  /**
-   * Add as a child of the specified element, removing from current parent if necessary
-   * @function addTo
-   * @memberof elation.ui.button#
-   * @returns {boolean}
-   */
-  this.addTo = function(parent) {
-    if (typeof parent != 'undefined') {
-      if (!this.container)
-        this.create();
-      parent.appendChild(this.container);
-      return true;
+    /**
+     * Sets the text label of the button
+     * @function setLabel
+     * @memberof elation.ui.button#
+     */
+    this.setLabel = function(label) {
+      this.label = label;
+      if (this.container)
+        this.container.innerHTML = label;
     }
-    return false;
-  }
-  /**
-   * Sets the text label of the button
-   * @function setLabel
-   * @memberof elation.ui.button#
-   */
-  this.setLabel = function(label) {
-    this.label = label;
-    if (this.container)
-      this.container.innerHTML = label;
-  }
-  /**
-   * Set whether the element is active or not
-   * @function setActive
-   * @memberof elation.ui.button#
-   * @param {boolean} active
-   */
-  this.setActive = function(active) {
-    if (active) {
-      elation.html.addclass(this.container, 'state_active');
-    } else {
-      elation.html.removeclass(this.container, 'state_active');
+    /**
+     * Sets the title text of the button
+     * @function setTitle
+     * @memberof elation.ui.button#
+     */
+    this.setTitle = function(title) {
+      if (this.container)
+        this.container.title = title;
     }
-  }
-  /**
-   * Event handler for HTML button's click event
-   * @function click
-   * @memberof elation.ui.button#
-   * @param {boolean} active
-   * @emits ui_button_click
-   */
-  this.click = function(ev) {
-    elation.events.fire({type: 'ui_button_click', element: this});
-    if (this.autoblur) {
-      this.container.blur();
+    /**
+     * Set whether the element is active or not
+     * @function setActive
+     * @memberof elation.ui.button#
+     * @param {boolean} active
+     */
+    this.setActive = function(active) {
+      if (active) {
+        elation.html.addclass(this.container, 'state_active');
+      } else {
+        elation.html.removeclass(this.container, 'state_active');
+      }
     }
-  }
-}, elation.ui.base);
+    /**
+     * Event handler for HTML button's click event
+     * @function click
+     * @memberof elation.ui.button#
+     * @param {boolean} active
+     * @emits ui_button_click
+     */
+    this.click = function(ev) {
+      elation.events.fire({type: 'ui_button_click', element: this});
+      if (this.autoblur) {
+        this.container.blur();
+      }
+    }
+  }, elation.ui.base);
+
+  elation.component.add('ui.togglebutton', function() {
+    this.init = function() {
+      elation.ui.togglebutton.extendclass.init.call(this);
+      this.toggletitle = this.args.toggletitle || false;
+      this.toggle(false);
+    }
+    this.toggle = function(newstate) {
+      if (newstate === undefined) newstate = !this.state;
+      this.state = newstate;
+      this.refresh();
+    }
+    this.render = function() {
+      var hasclass = this.hasclass('state_active');
+      if (!this.state && hasclass) {
+        this.removeclass('state_active');
+      } else if (this.state && !hasclass) {
+        this.addclass('state_active');
+      }
+
+      if (!this.state && this.title) {
+        this.setTitle(this.title);
+      } else if (this.state && this.toggletitle) {
+        this.setTitle(this.toggletitle);
+      }
+    }
+    /**
+     * Event handler for HTML button's click event
+     * @function click
+     * @memberof elation.ui.togglebutton#
+     * @param {event} ev
+     * @emits ui_button_click
+     */
+    this.click = function(ev) {
+      elation.events.fire({type: 'ui_button_click', element: this});
+      this.toggle();
+      if (this.autoblur) {
+        this.container.blur();
+      }
+    }
+  }, elation.ui.button);
+});
 
 /*
 plop = new function() {

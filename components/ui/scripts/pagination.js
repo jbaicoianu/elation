1bb7b196c813a5fead83c6ad028156b044c48bce
James Baicoianu <james_github@baicoianu.com>
2014-11-26 01:01:59 -0800

    Added base ui.pagination component

---
+++ components/ui/scripts/pagination.js
@@ -0,0 +1,18 @@
+elation.require([ ], function() {
+  elation.component.add('ui.pagination', function() {
+    this.defaultcontainer = {tag: 'ul', classname: 'ui_list ui_pagination'};
+
+    this.init = function() {
+      elation.ui.pagination.extendclass.init.call(this);
+      items = [];
+      items.push({label: '&laquo;', disabled: true});
+      for (var i = 1; i <= 5; i++) {
+        items.push({label: i});
+      }
+      items.push({label: '&raquo;'});
+      this.setItems(items);
+      this.setOrientation('horizontal');
+    }
+  }, elation.ui.list);
+});
+

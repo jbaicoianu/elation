9382d4145bf1d8d30e3f98c2a207f73e6abe4b36
James Baicoianu <james_github@baicoianu.com>
2014-12-02 23:27:59 -0800

    Moved ui.togglebutton into its own file

--- components/ui/scripts/button.js
+++ components/ui/scripts/button.js
@@ -122,47 +122,6 @@ elation.require(['ui.base'], function() {
       }
     }
   }, elation.ui.base);
-
-  elation.component.add('ui.togglebutton', function() {
-    this.init = function() {
-      elation.ui.togglebutton.extendclass.init.call(this);
-      this.toggletitle = this.args.toggletitle || false;
-      this.toggle(false);
-    }
-    this.toggle = function(newstate) {
-      if (newstate === undefined) newstate = !this.state;
-      this.state = newstate;
-      this.refresh();
-    }
-    this.render = function() {
-      var hasclass = this.hasclass('state_active');
-      if (!this.state && hasclass) {
-        this.removeclass('state_active');
-      } else if (this.state && !hasclass) {
-        this.addclass('state_active');
-      }
-
-      if (!this.state && this.title) {
-        this.setTitle(this.title);
-      } else if (this.state && this.toggletitle) {
-        this.setTitle(this.toggletitle);
-      }
-    }
-    /**
-     * Event handler for HTML button's click event
-     * @function click
-     * @memberof elation.ui.togglebutton#
-     * @param {event} ev
-     * @emits ui_button_click
-     */
-    this.click = function(ev) {
-      elation.events.fire({type: 'ui_button_click', element: this});
-      this.toggle();
-      if (this.autoblur) {
-        this.container.blur();
-      }
-    }
-  }, elation.ui.button);
 });
 
 /*

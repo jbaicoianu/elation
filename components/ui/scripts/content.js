d94cc86104a514292cc623a3afac175c0133e0e8
James Baicoianu <james_github@baicoianu.com>
2014-12-02 23:30:27 -0800

    Smarter content handling for ui.content

--- components/ui/scripts/content.js
+++ components/ui/scripts/content.js
@@ -12,6 +12,8 @@ elation.require("ui.base", function() {
   elation.component.add("ui.content", function() {
     this.defaultcontainer = {tag: 'div', classname: 'ui_content'};
 
+    this.content = '';
+
     this.init = function() {
       elation.ui.content.extendclass.init.call(this);
       if (this.args.content) {
@@ -24,7 +26,15 @@ elation.require("ui.base", function() {
       this.refresh();
     }
     this.render = function() {
-      this.container.innerHTML = this.content;
+      if (elation.utils.isString(this.content)) {
+        this.container.innerHTML = this.content;
+      } else if (this.content.container instanceof HTMLElement) {
+        this.container.innerHTML = '';
+        this.container.appendChild(this.content.container);
+      } else if (this.content instanceof HTMLElement) {
+        this.container.innerHTML = '';
+        this.container.appendChild(this.content);
+      }
     }
   }, elation.ui.base);
 });

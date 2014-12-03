53ebee78b0d40e5c9b57ea2f02d2adb2fe33eeb7
James Baicoianu <james_github@baicoianu.com>
2014-12-02 20:16:12 -0800

    Simple HTML string support, added .remove() and .clear()

--- components/ui/scripts/panel.js
+++ components/ui/scripts/panel.js
@@ -9,7 +9,7 @@
  * @param {string} args.classname
  * @param {string} args.orientation
  */
-elation.require("ui.base", function() {
+elation.require(["ui.base", "ui.content"], function() {
   elation.component.add("ui.panel", function() {
     this.defaultcontainer = {tag: 'div', classname: 'ui_panel'};
 
@@ -31,13 +31,30 @@ elation.require("ui.base", function() {
      */
     this.add = function(component) {
       if (component) {
-        this.container.appendChild(component.container);
+        if (elation.utils.isString(component)) {
+          var panel = elation.ui.content({content: component, append: this});
+          component = panel;
+        } else {
+          this.container.appendChild(component.container);
+        }
         this.items.push(component);
         return component;
       } else {
         console.log('Error: invalid component passed in to ui.panel.add');
       }
       return false;
+    }
+    this.remove = function(component) {
+      if (component.container && component.container.parentNode == this.container) {
+        this.container.removeChild(component.container);
+        var idx = this.items.indexOf(component);
+        if (idx != -1) this.items.splice(idx, 1);
+      }
+    }
+    this.clear = function() {
+      while (this.items.length > 0) {
+        this.remove(this.items[0]);
+      }
     }
   }, elation.ui.base);
 });

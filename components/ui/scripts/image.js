75ecf1c0537c93456a13c9a63eff945b95cc49d5
James Baicoianu <james_github@baicoianu.com>
2015-08-14 23:07:03 -0700

    Use propertyProxies for ui.image

--- components/ui/scripts/image.js
+++ components/ui/scripts/image.js
@@ -13,8 +13,9 @@ elation.require("ui.base", function() {
     this.defaultcontainer = {tag: 'img', classname: 'ui_image'};
 
     this.init = function() {
+      this.addPropertyProxies(['src', 'width', 'height']);
       if (this.args.src) {
-        this.container.src = this.args.src;
+        this.src = this.args.src;
       }
       if (this.args.classname) {
         elation.html.addclass(this.container, this.args.classname);

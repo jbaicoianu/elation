922b6fcc8a641c2e67042ecd1239bb6e629aef8f
James Baicoianu <james_github@baicoianu.com>
2016-04-18 01:55:43 -0700

    Prevent utils.pack from including config file twice

--- components/utils/scripts/pack.js
+++ components/utils/scripts/pack.js
@@ -10,12 +10,15 @@ elation.env.isWorker = false;
 
 var depname = process.argv[2];
 var requires = ['utils.events'];
-var config = false;
+var config = false,
+    bundle = 'bundle';
 if (process.argv.length > 2) {
   for (var i = 2; i < process.argv.length; i++) {
     var arg = process.argv[i];
     if (arg == '-config') {
       config = process.argv[++i];
+    } else if (arg == '-bundle') {
+      bundle = process.argv[++i];
     } else {
       requires.push(process.argv[i]);
     }
@@ -28,18 +31,23 @@ elation.requireCSS(requires);
 
 var resolved = elation.requireactivebatchjs.resolve(elation.requireactivebatchjs.rootnode);
 if (config) {
+  // If config is specified, make sure it's loaded immediately after utils.elation
+  var idx = -1;
+  resolved.forEach(function(n) { if (n.name == config) idx = resolved.indexOf(n); });
+  if (idx != -1) {
+    resolved.splice(idx, 1);
+  }
   resolved.unshift({name: config});
 }
 resolved.unshift({name: 'utils.elation'});
 
 var resolvednamesjs = [];
 resolved.forEach(function(r) { resolvednamesjs.push(r.name); });
-console.log('===== RESOLVED ORDER =====', resolvednamesjs);
+//console.log('===== RESOLVED ORDER =====', resolvednamesjs);
 var files = [];
 var sources = [];
 resolved.forEach(function(module) {
   var fname = 'htdocs/scripts/' + module.name.replace(/\./g, '/') + '.js';
-  console.log(fname);
   if (fs.existsSync(fname)) {
     files.push(fname);
     //var source = fs.readFileSync(fname, 'utf8');
@@ -67,7 +75,6 @@ if (elation.requireactivebatchcss) {
   resolvedcss.forEach(function(module) { 
     resolvednamescss.push(module.name);
     var fname = 'htdocs/css/' + module.name.replace(/\./g, '/') + '.css';
-    console.log(fname);
     if (fs.existsSync(fname)) {
       files.push(fname);
       var source = fs.readFileSync(fname, 'utf8');
@@ -75,12 +82,13 @@ if (elation.requireactivebatchcss) {
       csssources.push(source);
     }
   });
-  console.log('===== RESOLVED ORDER =====', resolvednamescss);
+  //console.log('===== RESOLVED ORDER =====', resolvednamescss);
 
-  fs.writeFileSync('bundle.css', csssources.join('\n'));
+  fs.writeFileSync(bundle + '.css', csssources.join('\n'));
   //sources.push('elation.requireactivebatchcss.fulfill(' + JSON.stringify(resolvednamescss) + ');');
   sources.splice(1, 0, 'elation.requireactivebatchcss = new elation.require.batch("css", "/css"); elation.requireactivebatchcss.fulfill(' + JSON.stringify(resolvednamescss) + ');');
   sources.splice(1, 0, 'elation.requireactivebatchjs = new elation.require.batch("js", "/scripts"); elation.requireactivebatchjs.fulfill(' + JSON.stringify(resolvednamesjs) + ');');
 }
-fs.writeFileSync('bundle.js', sources.join('\n'));
-console.log(elation.env);
+fs.writeFileSync(bundle + '.js', sources.join('\n'));
+
+console.log('Built files: ' + bundle + '.js ' + bundle + '.css');

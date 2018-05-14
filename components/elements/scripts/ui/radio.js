959180929463be0fc87d0120375cd07c0fdb0617
James Baicoianu <james_github@baicoianu.com>
2018-05-13 23:31:14 -0700

    Added preview state

--- components/elements/scripts/ui/radio.js
+++ components/elements/scripts/ui/radio.js
@@ -1,6 +1,9 @@
 elation.require(['elements.ui.toggle'], function() {
   elation.elements.define('ui.radio', class extends elation.elements.ui.toggle {
     create() {
+      if (this.preview) {
+        this.label = 'Radio Button';
+      }
       super.create();
       this.checkbox.type = 'radio';
     }

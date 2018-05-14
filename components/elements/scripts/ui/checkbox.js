e4c1ca9206ad793c26373ec51bdde0d6e6185740
James Baicoianu <james_github@baicoianu.com>
2018-05-13 23:27:17 -0700

    Added default preview options

--- components/elements/scripts/ui/checkbox.js
+++ components/elements/scripts/ui/checkbox.js
@@ -1,5 +1,11 @@
 elation.require(['elements.ui.toggle'], function() {
   elation.elements.define('ui.checkbox', class extends elation.elements.ui.toggle {
+    init() {
+      super.init();
+      if (this.preview) {
+        this.label = 'Checkbox';
+      }
+    }
   });
 });
 

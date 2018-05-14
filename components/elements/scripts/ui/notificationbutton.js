e241557b4f2c6969efa05894f1166fc08bf254a1
James Baicoianu <james_github@baicoianu.com>
2018-05-13 23:30:14 -0700

    Added preview state

--- components/elements/scripts/ui/notificationbutton.js
+++ components/elements/scripts/ui/notificationbutton.js
@@ -5,6 +5,10 @@ elation.require(['elements.ui.button', 'elements.ui.indicator'], function() {
       this.defineAttributes({
         count: { type: 'integer', default: 0, set: this.updateCount }
       });
+      if (this.preview) {
+        this.count = 3;
+        this.label = 'Notifications';
+      }
     }
     create() {
       super.create();

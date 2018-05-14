3b7491ac8a2fcc02c8b75b9b0154b9f9b2ef6b7a
James Baicoianu <james_github@baicoianu.com>
2018-05-13 23:28:12 -0700

    Support labels for formgroups

--- components/elements/scripts/ui/formgroup.js
+++ components/elements/scripts/ui/formgroup.js
@@ -1,6 +1,19 @@
 elation.require(['elements.elements'], function() {
   elation.elements.define('ui.formgroup', class extends elation.elements.base {
+    init() {
+      super.init();
+      this.defineAttributes({
+        'label': { type: 'text' }
+      });
+    }
     create() {
+      if (this.label) {
+        this.labelobj = elation.elements.create('ui-label', {
+          label: this.label,
+          append: this,
+          class: 'groupheader'
+        });
+      }
     }
   });
 });

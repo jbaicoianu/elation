04ff7c914743df56d5b7109f897af92f04e418d0
James Baicoianu <james_github@baicoianu.com>
2018-05-13 23:58:15 -0700

    panel.setcontent(), don't update layout if not in DOM, recalculate layout for more events

--- components/elements/scripts/ui/panel.js
+++ components/elements/scripts/ui/panel.js
@@ -1,6 +1,7 @@
 elation.require(['elements.elements'], function() {
   elation.elements.define('ui.panel', class extends elation.elements.base {
     init() { 
+      super.init();
       this.defineAttributes({
         top:    {type: 'boolean', default: false, set: this.updateLayout },
         middle: {type: 'boolean', default: false, set: this.updateLayout },
@@ -18,14 +19,23 @@ elation.require(['elements.elements'], function() {
       observer.observe(this, {attributes: false, childList: true, subtree: true});
 
       // Update our layout if the page is resized or if orientation changes
+      document.addEventListener('DOMContentLoaded', (ev) => this.refresh());
+      document.addEventListener('load', (ev) => this.refresh());
       window.addEventListener('resize', (ev) => this.refresh());
       window.addEventListener('orientationchange', (ev) => this.refresh());
+      this.refresh();
+      // FIXME - sometimes panels initialize too early, and their vertical positioning gets messed up.  A 10ms timeout helps but an event would be better.
+      setTimeout(() => {
+        this.refresh();
+      }, 10);
     }
     render() {
       super.render();
       this.updateLayout();
     }
     updateLayout() {
+      if (!this.offsetParent) return; // Not in the DOM, no layout to be done
+
       if (this.middle) {
         this.style.top = ((this.offsetParent.offsetHeight - this.offsetHeight) / 2) + 'px';
         this.style.bottom = 'auto';
@@ -45,6 +55,13 @@ elation.require(['elements.elements'], function() {
       } else if (this.right) {
         this.style.left = (this.left ? 0 : 'auto');
         this.style.right = 0;
+      }
+    }
+    setcontent(content) {
+      if (elation.utils.isString(content)) {
+        this.innerHTML = content;
+      } else if (content instanceof HTMLElement) {
+        this.appendChild(content);
       }
     }
   });

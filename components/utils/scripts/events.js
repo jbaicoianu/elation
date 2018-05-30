536b665dd22dd5945198d30ffef96f17349b1e8d
James Baicoianu <james_github@baicoianu.com>
2018-05-30 03:45:30 -0700

    Added mousewheel delta attributes

--- components/utils/scripts/events.js
+++ components/utils/scripts/events.js
@@ -1,7 +1,7 @@
 // if (typeof require == 'function') var elation = require("utils/elation");
 elation.extend("events", {
   events: {},
-  cloneattrs: ['type', 'bubbles', 'cancelable', 'view', 'detail', 'screenX', 'screenY', 'clientX', 'clientY', 'ctrlKey', 'shiftKey', 'altKey', 'metaKey', 'button', 'relatedTarget', 'target', 'element', 'data', 'origin', 'timeStamp', 'returnValue', 'cancelBubble', 'keyCode', 'dataTransfer'],
+  cloneattrs: ['type', 'bubbles', 'cancelable', 'view', 'detail', 'screenX', 'screenY', 'clientX', 'clientY', 'ctrlKey', 'shiftKey', 'altKey', 'metaKey', 'button', 'relatedTarget', 'target', 'element', 'data', 'origin', 'timeStamp', 'returnValue', 'cancelBubble', 'keyCode', 'dataTransfer', 'deltaX', 'deltaY', 'deltaZ', 'deltaMode'],
 
   eventstats: {},
   

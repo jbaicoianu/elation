299b407cabd90d08e9520006914d43029c636384
James Baicoianu <james_github@baicoianu.com>
2022-05-02 01:44:12 -0700

    Support for ui.popupbutton with detached popup

--- components/elements/scripts/ui/popupbutton.js
+++ components/elements/scripts/ui/popupbutton.js
@@ -5,6 +5,7 @@ elation.require(['elements.ui.button'], function() {
       this.defineAttributes({
         active: { type: 'boolean', default: false },
         popupcontent: { type: 'object' },
+        detached: { type: 'boolean', default: false },
       });
     }
     create() {
@@ -26,7 +27,7 @@ elation.require(['elements.ui.button'], function() {
     }
     handleWindowClick(ev) {
       // Separate click handler to hide the window if the user clicks anywhere in the window that's not inside the popup window
-      if (!elation.utils.isin(this, ev.target)) {
+      if (!(elation.utils.isin(this, ev.target) || elation.utils.isin(this.popup, ev.target))) {
         this.hidePopup();
       }
     }
@@ -36,18 +37,31 @@ elation.require(['elements.ui.button'], function() {
         content = elation.elements.create('ui-content');
         content.fromString(this.popupcontent);
       }
-      this.popup = elation.elements.create('ui.window', {
-        append: this,
-        //title: 'Sounds',
-        movable: false, 
-        controls: false,
-        minimizable: '0', 
-        maximizable: '0', 
-        closable: '0', 
-        resizable: '0', 
-        bottom: '40px',
-        left: 0,
-      });
+      if (this.detached) {
+        let pos = this.getBoundingClientRect();
+        this.popup = elation.elements.create('ui.window', {
+          append: document.body,
+          movable: false,
+          controls: false,
+          minimizable: '0',
+          maximizable: '0',
+          closable: '0',
+          resizable: '0',
+          center: true,
+        });
+      } else {
+        this.popup = elation.elements.create('ui.window',
+          append: this,
+          movable: false,
+          controls: false,
+          minimizable: '0',
+          maximizable: '0',
+          closable: '0',
+          resizable: '0',
+          bottom: '40px',
+          left: 0,
+        });
+      }
       this.popup.setcontent(content);
       //this.popup.open();
       this.windowClickHandler = (ev) => this.handleWindowClick(ev); // bind event so it can be added and removed
@@ -56,14 +70,19 @@ elation.require(['elements.ui.button'], function() {
     showPopup() {
       if (!this.popup) {
         this.createPopup();
-      } else if (this.popup.parentNode !== this) {
-        this.appendChild(this.popup);
+      } else if (!this.popup.parentNode) {
+        if (this.detached) {
+          document.body.appendChild(this.popup);
+          setTimeout(() => this.popup.refresh(), 0);
+        } else {
+          this.appendChild(this.popup);
+        }
         window.addEventListener('click', this.windowClickHandler);
       }
     }
     hidePopup() {
-      if (this.popup.parentNode === this) {
-        this.removeChild(this.popup);
+      if (this.popup && this.popup.parentNode) {
+        this.popup.parentNode.removeChild(this.popup);
         window.removeEventListener('click', this.windowClickHandler);
       }
     }
